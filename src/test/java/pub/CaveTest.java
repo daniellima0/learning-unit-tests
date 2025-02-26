a74506d0afd1d442b53d38ada5882ef48931031b
Daniel Lima <danielmlimag@gmail.com>
2025-02-26 09:12:16 +0100

    Finish tests without changing original code

--- src/test/java/pub/CaveTest.java
+++ src/test/java/pub/CaveTest.java
@@ -3,7 +3,6 @@ package pub;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.util.List;
 
 class CaveTest {
 
